6c1348248f98550e60708c672d2c80ef0e6474b9
edwar-vhd <lyriard@gmail.com>
2022-12-10 21:43:46 -0500

    apple added

--- snake.js
+++ snake.js
@@ -1,5 +1,12 @@
 var canvas = document.getElementById("canvas");
+var scoreLabel = document.getElementById("score");
 var keyCode = [];
+var score = 0;
+var animation = 0;
+
+var snake;
+var apple;
+
 const ctx = canvas.getContext("2d");
 const mapSideParts = 31;
 
@@ -22,6 +29,7 @@ class Snake{
         });
         this.moveX = 0;
         this.moveY = 0;
+        this.isAlive = true;
     }
 
     // Metodos
@@ -102,11 +110,37 @@ class Snake{
         }
     }
 
-    // Getters and setters
+    crash(){
+        for(var i=1; i<this.body.length; i++){
+            if(this.body[i].x == this.body[0].x && this.body[i].y == this.body[0].y){
+                this.isAlive = false;
+            }
+        }
+    }
 }
 
-var snake = new Snake(Math.round(mapSideParts/2)-1,Math.round(mapSideParts/2)-1);
-//var apple = new Apple();
+class Apple{
+    // Constructor
+    constructor(){
+        while(true){
+            let count=0;
+
+            this.x = Math.floor(Math.random() * mapSideParts);
+            this.y = Math.floor(Math.random() * mapSideParts);
+            
+            for(var i=0; i<snake.body.length; i++){
+                if(snake.body[i].x == this.x && snake.body[i].y == this.y){
+                    break;
+                }else{
+                    count++;
+                }
+            }
+            if (count == snake.body.length){
+                break;
+            }
+        }
+    }
+}
 
 window.onload = () => {
     gameLoop();
@@ -120,9 +154,9 @@ function gameLoop(){
 function draw(){
     let mapSideLenght;
 
-    if (document.documentElement.getBoundingClientRect().width < document.documentElement.getBoundingClientRect().height){
+    if(document.documentElement.getBoundingClientRect().width < document.documentElement.getBoundingClientRect().height){
         mapSideLenght = document.documentElement.getBoundingClientRect().width - 120;
-        
+
     }else{
         mapSideLenght = document.documentElement.getBoundingClientRect().height - 120;
     }
@@ -145,13 +179,77 @@ function draw(){
             squareSize-(squareSize/8)*2,
             squareSize-(squareSize/8)*2);
     }
+
+    // Apple
+    ctx.shadowColor = "#FF49AD";
+    ctx.shadowBlur = squareSize/6;
+    ctx.fillStyle = "#FF49AD";
+    ctx.fillRect(
+        apple.x*squareSize+(squareSize/8),
+        apple.y*squareSize+(squareSize/8),
+        squareSize-(squareSize/8)*2,
+        squareSize-(squareSize/8)*2);
+
+
+    if(!snake.isAlive){
+        ctx.shadowBlur = 0;
+        ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+        ctx.fillRect(0,0,mapSideLenght,mapSideLenght);
+
+        ctx.shadowColor = "#FFFFFF";
+        ctx.shadowBlur = squareSize/8;
+
+        ctx.font = "80px Arial";
+        ctx.fillStyle = "#ffffff";
+        ctx.textAlign = 'center';
+        ctx.fillText("Game Over",mapSideLenght/2,mapSideLenght/2);
+
+        animation++;
+        if(animation<30){
+            ctx.font = "20px Arial";
+            ctx.fillText("Press spacebar to continue...",mapSideLenght/2,mapSideLenght/2+30);
+        }
+        if (animation>=45){
+            animation=0;
+        }
+    }
 }
 
 function game(){
-    snake.move();
+    console.log(snake.isAlive);
+    if (snake.isAlive){
+        snake.move();
+        snake.crash();
+        eat();
+    }
+}
+
+function eat(){
+    if(snake.body[0].x == apple.x && snake.body[0].y == apple.y){
+        snake.body.unshift({
+            x: apple.x,
+            y: apple.y
+        });
+        score++;
+        scoreLabel.innerHTML = "Score: "+score;
+        apple = new Apple();
+    }
 }
 
 window.addEventListener("keydown", (event) => {
-    keyCode.push(event.keyCode);
-    console.log(keyCode);
-});
+    if(event.keyCode==32 && !snake.isAlive){
+        snake.moveX=0;
+        snake.moveY=0;
+        snake = new Snake(Math.round(mapSideParts/2)-1,Math.round(mapSideParts/2)-1);
+        apple = new Apple();
+
+        keyCode = [];
+        score=0;
+        scoreLabel.innerHTML = "Score: "+score;
+    }else{
+        keyCode.push(event.keyCode);
+    }
+},false);
+
+snake = new Snake(Math.round(mapSideParts/2)-1,Math.round(mapSideParts/2)-1);
+apple = new Apple();
